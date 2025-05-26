ea935652cf06b56cfd30b59abbe30851110ffcd5
SHazdeApp <shazdeapp@gmail.com>
2025-05-26 09:19:34 +0330

    new game

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1748238317,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/outrun_test.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/start_spr-sheet0.png",
+		"media/spiral_-_act_of_impulse_phenomenalretromix.mp3.m4a",
+		"media/spiral_-_act_of_impulse_phenomenalretromix.mp3.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"mute.png",
+		"background.png",
+		"background2.png",
+		"spr2.png",
+		"sprites.png"
+	]
+}
